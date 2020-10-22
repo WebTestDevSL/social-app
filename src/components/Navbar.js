0d80817b6b09d130df3a91eefce2f414e51b8b29
Dulaj Prabasha <dulaj@Dulajs-MacBook-Pro.local>
2020-10-22 14:26:44 +0530

    Add wireframe

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,35 +6,22 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <div className="Navbar mx-auto bg-indigo-100 p-5">
-      <nav className="flex justify-between  px-10">
+      <nav className="flex justify-between px-10">
         <div className="flex flex-row">
           <a className="pr-5" href="#">
             Logo
           </a>
           <form>
-            <input
-              type="text"
-              id="new-todo-input"
-              className="input input__lg"
-              name="text"
-              autoComplete="off"
-              size="30"
-            />
+            <input type="text" name="text" autoComplete="off" size="30" />
           </form>
         </div>
-        <div className="flex flex-row">
-          <ul className="flex flex-row">
-            <li className="pr-5">
-              <a> Sadie </a>
-            </li>
-            <li className="pr-5">
-              <a> Home </a>
-            </li>
-            <li className="pr-5">
-              <a> Create </a>
-            </li>
-          </ul>
-        </div>
+
+        <ul className="flex flex-row">
+          <li className="pr-5">Sadie</li>
+          <li className="pr-5">Home</li>
+          <li className="pr-5">Create</li>
+        </ul>
+
         <ul className="flex flex-row">
           <li className="pr-6">
             <BsBell className="react-icons" />
